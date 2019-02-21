7dc6b06ebc57674019bd452e6a45d70ff3e1053a
alekseik1 <alekseik1@yandex.ru>
2019-02-22 00:30:54 +0300

    Add `insert_data` SQL commands

--- Kozharin Aleksei/homework2/insert_data.sql
+++ Kozharin Aleksei/homework2/insert_data.sql
@@ -1,3 +1,7 @@
-LOAD INPATH ...
+-- .csv files are in /var/lib/mysql-files/ -> it is 'secure' folder
+LOAD DATA INFILE '/var/lib/mysql-files/users.csv' INTO TABLE users FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
-INSERT INTO TABLE ...
+LOAD DATA INFILE '/var/lib/mysql-files/sessions.csv' INTO TABLE sessions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE '/var/lib/mysql-files/payments.csv' INTO TABLE payments FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
