83db9006a8d85ce1a56f6dbbd59c75af6ec9b661
Kyzyl-ool <kyzyloolk@mail.ru>
2019-03-04 17:03:27 +0300

    Revert "Homework 3"
    This reverts commit 15ed47476e453e708683fbed3c624e01dfbd61a7.

--- Kyzyl-ool Kezhik/homework3/selects.sql
+++
@@ -1,8 +0,0 @@
-select users.user_id, sum(payment_sum)
-from users JOIN payments ON users.user_id = payments.user_id
-order by 2 desc
-limit 3;
-
-select t1.session_amount/t2.users_amount as average_sessions_for_user from
-    (select count(0) as session_amount from sessions) as t1 join
-    (select count(1) as users_amount from users) as t2 on true;
