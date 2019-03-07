3c588815e9a7302e399c25bed0175a7b45985cf2
Denis Stasyev <denisstasyev@gmail.com>
2019-03-07 14:50:34 +0300

    ppu added

--- Stasyev Denis/homework4/ppu_select.sql
+++ Stasyev Denis/homework4/ppu_select.sql
@@ -1,5 +1,28 @@
 -- PPU – Percentage of Paying Users – доля платящей аудитории относительно DAU
 
-SELECT ...
+USE `technotrack_default`;
 
-> результаты...
+WITH RECURSIVE cte AS (
+    SELECT MIN(CAST(`payment_dttm` AS DATE)) AS dt FROM `payments`
+        UNION ALL
+	SELECT dt + INTERVAL 1 DAY
+    FROM cte
+    WHERE dt + INTERVAL 1 DAY <= (SELECT MAX(CAST(`payment_dttm` AS DATE)) FROM `payments`)
+),
+pu_per_day AS (
+    SELECT CAST(`payment_dttm` AS DATE) AS dt,
+        COUNT(DISTINCT `user_id`) as users_paying
+    FROM `payments`
+    GROUP BY dt
+),
+dau_per_day AS (
+    SELECT CAST(`begin_dttm` AS DATE) AS dt,
+        COUNT(DISTINCT `user_id`) as users_all
+    FROM `sessions`
+    GROUP BY dt
+)
+SELECT cte.dt AS `Date`, 
+       COALESCE(pu.users_paying, 0) / COALESCE(dau.users_all, 1) AS `PPU – Percentage of Paying Users`
+FROM cte
+LEFT JOIN pu_per_day AS pu ON cte.dt = pu.dt
+LEFT JOIN dau_per_day AS dau ON cte.dt = dau.dt;
