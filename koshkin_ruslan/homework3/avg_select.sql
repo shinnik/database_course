ec606a22b0dffc3528836fcd2e4d053448c78815
Ruslan <ruslanbk11@mail.ru>
2019-03-07 16:34:08 +0300

    Added results of queries

--- koshkin_ruslan/homework3/avg_select.sql
+++ koshkin_ruslan/homework3/avg_select.sql
@@ -2,3 +2,6 @@ select avg(num) from
 (select user_id, count(session_id) as num
 	from sessions
 	group by user_id) as n;
+
+--  67.7547 
+
--- koshkin_ruslan/homework3/top_select.sql
+++ koshkin_ruslan/homework3/top_select.sql
@@ -4,3 +4,8 @@ select login from users,
 	group by user_id 
 	order by sum(payment_sum) desc limit 3) as s 
 where users.user_id=s.user_id;
+
+-- login_82 
+-- login_38 
+-- login_85 
+
