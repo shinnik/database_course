1e5dbb8998fb0fe13046f68c88b3452609b4279e
theflower86 <theflower86@mail.ru>
2019-03-07 10:00:30 +0300

    hw3: Mironova: results added

--- Mironova_Liliya/homework3/avg_select.sql
+++ Mironova_Liliya/homework3/avg_select.sql
@@ -1,1 +1,7 @@
 SELECT AVG(cnt) FROM (SELECT COUNT(session_id) AS cnt FROM sessions group by user_id) CntTable;
+
++----------+
+| AVG(cnt) |
++----------+
+|  67.7547 |
++----------+
--- Mironova_Liliya/homework3/top_select.sql
+++ Mironova_Liliya/homework3/top_select.sql
@@ -1,1 +1,9 @@
 SELECT distinct login FROM users AS u JOIN (select id, sum(payment_sum)/2 as psum from (select id, login, payment_sum from users left join payments on users.user_id = payments.user_id)ps group by id order by psum desc limit 3)p on u.user_id = p.user_id;
+
++----------+
+| login    |
++----------+
+| login_38 |
+| login_82 |
+| login_85 |
++----------+
